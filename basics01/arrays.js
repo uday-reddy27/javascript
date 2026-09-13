809dcff49748a09847abec69ed0310a072276cc5
UDAY REDDY KANCHARLA <kancharlaudayreddy12345@gmail.com>
2026-09-13 06:43:07 +0000

    array done

---
+++ basics01/arrays.js
@@ -0,0 +1,22 @@
+let arr = [1,2,3,4,5,6];
+
+arr.push(7);
+
+console.log(arr);
+
+console.log(arr.length);
+
+arr.pop()
+
+console.log(arr)
+
+arr.unshift(10,11,12);
+
+console.log(arr)
+
+arr.shift();
+
+console.log(arr);
+
+let newArr = arr.join();
+console.log(newArr);
